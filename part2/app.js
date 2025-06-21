6f05267cbe9e638df95646374c5fef3532f8f556
YiTongCai <161269135+YiTongCai@users.noreply.github.com>
2025-06-21 16:11:46 +0930

    Update app.js

--- part2/app.js
+++ part2/app.js
@@ -1,3 +1,38 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mysql = require('mysql2/promise');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+let db;
+
+(async () => {
+  try {
+    const connection = await mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: ''
+    });
+    await connection.query('CREATE DATABASE IF NOT EXISTS testdb');
+    await connection.end();
+    db = await mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'testdb'
+    });
+  } catch (err) {
+    console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
+  }
+})();
+
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
@@ -14,6 +49,16 @@ const userRoutes = require('./routes/userRoutes');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
+
+app.post('/logout',(req, res) => {
+ req.session.destroy(err=>{
+    if(err){
+        return res.status(500).send("Logout failed.");l
+    }
+    res.clearCookie('connect.sid');
+    res.redirect('/')
+ })
+});
 
 // Export the app instead of listening here
 module.exports = app;
